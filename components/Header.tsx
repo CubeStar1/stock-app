1cebcdace9e64499728565de58a3f155e730d722
CubeStar1 <avinash.anish04@gmail.com>
2024-08-01 19:46:01 +0530

    Search button

--- components/Header.tsx
+++ components/Header.tsx
@@ -21,7 +21,7 @@ const Header: React.FC<HeaderProps> = ({ onMenuClick }) => (
       </div>
       </a>
     </div>
-    <div className="flex items-center space-x-4">
+    <div className="flex items-center space-x-4 ">
       <StockSearch />
       <Bell className="w-5 h-5 text-gray-500 cursor-pointer" />
       <User className="w-5 h-5 text-gray-500 cursor-pointer" />
--- components/StockSearch.tsx
+++ components/StockSearch.tsx
@@ -4,6 +4,16 @@ import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { Search } from 'lucide-react';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
 
 const StockSearch: React.FC = () => {
   const [query, setQuery] = useState('');
@@ -17,16 +27,48 @@ const StockSearch: React.FC = () => {
   };
 
   return (
-    <form onSubmit={handleSearch} className="flex">
-      <Input
-        type="text"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        placeholder="Enter stock symbol (e.g., AAPL)"
-        className="w-64 mr-2"
-      />
-      <Button type="submit">Search</Button>
-    </form>
+    <>
+
+      <div className='lg:hidden'>
+        <Dialog>
+          <DialogTrigger>
+            <Search className="w-5 h-5 text-gray-500 cursor-pointer" />
+          </DialogTrigger>
+          <DialogContent className='p-4 rounded-lg'>
+            <DialogHeader>
+              <DialogTitle>
+                Search Stocks
+              </DialogTitle>
+            </DialogHeader>
+            <DialogDescription>
+              <form onSubmit={handleSearch} className="flex justify-between">
+                <Input
+                  type="text"
+                  value={query}
+                  onChange={(e) => setQuery(e.target.value)}
+                  placeholder="Enter stock symbol (e.g., AAPL)"
+                  className="w-full mr-2"
+                />
+                <Button type="submit">Search</Button>
+              </form>
+            </DialogDescription>
+          </DialogContent>
+        </Dialog>
+      </div>
+      <div className='hidden lg:block'>
+        <form onSubmit={handleSearch} className="flex">
+          <Input
+            type="text"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            placeholder="Enter stock symbol (e.g., AAPL)"
+            className="w-64 mr-2"
+          />
+          <Button type="submit">Search</Button>
+        </form>
+      </div>
+    
+    </>
   );
 };
 
